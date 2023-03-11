dc2b28c630264feb9fa275182741ac8b81176817
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-11 12:47:18 -0800

    ⚗️ Try other command

--- cli_for_tools/example_hello.py
+++ cli_for_tools/example_hello.py
@@ -7,3 +7,7 @@ app = typer.Typer(help="Awesome CLI user manager.")
 def main(name: str):
     print(f"Hello {name}")
 
+
+@app.command()
+def good_bye(name: str = "Chato"):
+    print(f"adios {name}")
