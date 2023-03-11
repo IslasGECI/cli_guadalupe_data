106454b6ff5209b524c7cd30986635a288c1cf48
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-11 12:17:02 -0800

    🎨 Format with Black

--- cli_for_tools/example_hello.py
+++ cli_for_tools/example_hello.py
@@ -1,12 +1,13 @@
 import click
 
+
 @click.group()
 def cli():
     pass
+
 
 @cli.command(short_help="Cálcula la distribución posterior para el tamaño de la población inicial")
 @click.option("--name", "-n", type=str, help="Nombre del recurso csv")
 def main(**arguments):
     name = arguments["name"]
     print(f"Hello {name}")
-
