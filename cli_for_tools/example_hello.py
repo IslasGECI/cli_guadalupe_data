66ea7d6d4057c2a3cb38a031783665ffaff16456
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-11 12:16:20 -0800

    ⚗️ Try using Click

--- cli_for_tools/example_hello.py
+++ cli_for_tools/example_hello.py
@@ -1,9 +1,12 @@
-import typer
+import click
 
+@click.group()
+def cli():
+    pass
 
-def main(name: str):
+@cli.command(short_help="Cálcula la distribución posterior para el tamaño de la población inicial")
+@click.option("--name", "-n", type=str, help="Nombre del recurso csv")
+def main(**arguments):
+    name = arguments["name"]
     print(f"Hello {name}")
 
-
-if __name__ == "__main__":
-    typer.run(main)
