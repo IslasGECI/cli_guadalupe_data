0d16fdd6eeb4166aeb8cc0ad889b2c331d21b880
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-11 12:28:41 -0800

    ⚗️ Try again with typer

--- cli_for_tools/example_hello.py
+++ cli_for_tools/example_hello.py
@@ -1,13 +1,9 @@
-import click
+import typer
+
+app = typer.Typer(help="Awesome CLI user manager.")
 
 
-@click.group()
-def cli():
-    pass
-
-
-@cli.command(short_help="Cálcula la distribución posterior para el tamaño de la población inicial")
-@click.option("--name", "-n", type=str, help="Nombre del recurso csv")
-def main(**arguments):
-    name = arguments["name"]
+@app.command()
+def main(name: str):
     print(f"Hello {name}")
+
