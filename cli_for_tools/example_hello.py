d1b971371b3fb18db10c267ca572c949264b8485
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-13 07:20:47 -0700

    🚧 Add function to clean directory data

--- cli_for_tools/example_hello.py
+++ cli_for_tools/example_hello.py
@@ -3,6 +3,12 @@ import typer
 app = typer.Typer(help="Awesome CLI user manager.")
 
 
+def clean_data_when_use_yolo():
+    sudo chown --recursive $USER:$USER .
+    rm --force --recursive raw
+    rm --force --recursive resized
+
+
 @app.command()
 def say_hi(name: str):
     print(f"Hello {name}")
