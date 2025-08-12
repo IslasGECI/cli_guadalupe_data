c8d5cd1d677045d068b3e4e2a11bf7ac620eff11
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-12 21:24:28 +0000

    🛑🧪 Fail tests

--- tests/test_paco_cli.py
+++ tests/test_paco_cli.py
@@ -1,7 +1,21 @@
-import cli_for_tools as dt
+from cli_for_tools.paco_cli import app
+import cli_for_tools as ct
+from typer.testing import CliRunner
+
+runner = CliRunner()
+
+
+def test_app():
+    result = runner.invoke(
+        app,
+        ["haz-mapa", "--help"],
+    )
+    assert result.exit_code == 0
+    assert "positions_path" in result.stdout
+    assert "mapsource_path" in result.stdout
 
 
 def test_version():
     expected = "0.2.0"
-    obtained = dt.__version__
+    obtained = ct.__version__
     assert expected == obtained
